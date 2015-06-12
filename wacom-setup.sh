7ecb3ee3c9a6b128efdb877fa871a6c63c072f24
Ilya Portnov <portnov@iportnov.ru>
2015-06-13 00:26:47 +0500

    Minor update.

--- wacom-setup.sh
+++ wacom-setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-chmod -v a+w /sys/bus/usb/devices/*/*/wacom_led/status_led0_select >> /var/log/wacom
+COUNT=10
+FOUND=0
+while [ $COUNT -gt 0 ]
+do if [ -f /sys/bus/usb/devices/*/*/wacom_led/status_led0_select ]
+   then FOUND=1
+        break
+   else echo "File does not exist yet, waiting" >> /var/log/wacom
+        sleep 1s
+   fi
+   COUNT=$((COUNT-1))
+done
+
+if [ $FOUND = 1 ]
+then chmod -v a+w /sys/bus/usb/devices/*/*/wacom_led/status_led0_select >> /var/log/wacom
+else echo File was not found for 10 seconds, exiting >> /var/log/wacom
+fi
 
