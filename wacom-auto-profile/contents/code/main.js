7a0b06333dfffaa3c6bcd9012b40e37c317b516d
Ilya Portnov <portnov@bk.ru>
2015-06-15 23:47:58 +0500

    Fix kwinscript.

--- wacom-auto-profile/contents/code/main.js
+++ wacom-auto-profile/contents/code/main.js
@@ -1,3 +1,34 @@
 
-callDBus("org.kde.Wacom", "/Tablet", "org.kde.Wacom", "getTabletList", callback=function(tablets) {
-  if (tablets) {
+function setProfile(newProfile) {
+    callDBus("org.kde.Wacom", "/Tablet", "org.kde.Wacom", "getTabletList", callback=function(tablets) {
+    if (tablets) {      
+        callDBus("org.kde.Wacom", "/Tablet", "org.kde.Wacom", "setProfile", ""+tablets, newProfile);
+    } else {
+        print("No tablets detected");
+    }
+    });
+}
+
+var profiles = {
+    'Krita': 'Krita',
+    'gimp': 'Gimp'
+};
+
+function onFocus(client) {
+    if (! client || typeof(client) == 'undefined') {
+      return;
+    }
+    var found = false;
+    for (cls in profiles) {
+        if (client.resourceClass == cls) {
+            found = true;
+            setProfile(profiles[cls]);
+            break;
+        }
+    }
+    if (! found) {
+      setProfile("Default");
+    }
+}
+
+workspace.clientActivated.connect(onFocus);
