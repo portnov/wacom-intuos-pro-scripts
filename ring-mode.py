ca85e4aa33a2222d3141e9ab9347ef2ab6c669ed
Ilya Portnov <portnov@iportnov.ru>
2015-06-17 21:46:31 +0500

    Update python script.

--- ring-mode.py
+++ ring-mode.py
@@ -39,10 +39,6 @@ def get_mode(path):
     with open(path, 'r') as f:
         return int(f.read())
 
-def set_mode(path, mode):
-    with open(path, 'w') as f:
-        f.write(str(mode))
-
 def notify(text):
     if pynotify_available:
         try:
@@ -54,6 +50,7 @@ def notify(text):
 
 def xsetwacom(args):
     command = "xsetwacom " + args
+    print(command)
     p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, close_fds=True)
     return p
 
@@ -68,6 +65,15 @@ def get_pad_devicename():
 
 def assign_tablet_button(pad, button, action):
     xsetwacom("set \"{}\" {} \"{}\"".format(pad, button, action))
+
+def set_mode(path, mode, mode_idx):
+    pad = get_pad_devicename()
+    if 'ring_down' in mode:
+        assign_tablet_button(pad, 'AbsWheelDown', mode['ring_down'])
+    if 'ring_up' in mode:
+        assign_tablet_button(pad, 'AbsWheelUp', mode['ring_up'])
+    with open(path, 'w') as f:
+        f.write(str(mode_idx))
 
 def get_profile():
     # Create a session bus.
@@ -84,8 +90,15 @@ def get_profile():
         profile = iface.getProfile()
     return str(profile)
 
+def load_config():
+    if len(sys.argv) == 2:
+        config_path = sys.argv[1]
+    else:
+        config_path = os.path.expanduser("~/.config/wacom-ring-modes.yaml")
+    config = yaml.safe_load(open(config_path,'r').read())
+    return config
 
-def toggle_mode(nmodes=4):
+def toggle_mode(config):
     path = get_control_file_path()
     if not path:
         print("Wacom tablet does not appear to be attached.")
@@ -94,12 +107,28 @@ def toggle_mode(nmodes=4):
         print("You have no write permission to Wacom LED control file {}.".format(path))
         sys.exit(2)
 
-    old_mode = get_mode(path)
-    new_mode = (old_mode + 1) % nmodes
-    set_mode(path, new_mode)
+    profile = get_profile()
+    config = load_config()
+    if profile not in config:
+        if 'Default' in config:
+            print("Profile {} is not described in the config. Using default profile.".format(profile))
+            profile = 'Default'
+        else:
+            print("Profile {} is not described in the config, and the Default profile is not described either.".format(profile))
+            sys.exit(3)
+
+    modes = config[profile]
+    nmodes = len(modes)
+
+    old_mode_idx = get_mode(path)
+    old_mode = modes[old_mode_idx].get("description", str(old_mode_idx))
+    new_mode_idx = (old_mode_idx + 1) % nmodes
+    new_mode = modes[new_mode_idx].get("description", str(new_mode_idx))
+    set_mode(path, modes[new_mode_idx], new_mode_idx)
     notify("Mode switched from {} to {}".format(old_mode, new_mode))
 
 if __name__ == "__main__":
 
-    toggle_mode()
+    config = load_config()
+    toggle_mode(config)
 
