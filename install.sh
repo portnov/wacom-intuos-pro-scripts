1f2c4579897199297b6c4087431463bc3b6c46ba
Ilya Portnov <portnov@iportnov.ru>
2015-06-17 21:50:11 +0500

    Reorganize files.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
-sudo cp wacom-setup.sh /usr/local/sbin/
-sudo cp wacom-ring-mode.sh /usr/local/bin/
-sudo cp wacom-keys.sh /usr/local/bin/
-sudo cp wacom-switch-mode.sh /usr/local/bin/
-sudo cp 99-local.rules /lib/udev/rules.d/
+sudo cp udev/wacom-setup.sh /usr/local/sbin/
+sudo cp udev/99-local.rules /lib/udev/rules.d/
+
+sudo cp python/ring-mode.py /usr/local/bin/
